df7fd6b5d1c36a5828d06386dbfffd0b4be9bbb1
Tepen <elcabron.penyah@gmail.com>
2020-09-05 00:48:06 +0800

    consumed api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
 	devise_scope :user do  
    get '/users/sign_out' => 'devise/sessions#destroy'
 end
+
+  get '/v1/accounts/' => 'accounts#test'
+  get '/v1/accounts/:id' => 'accounts#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "posts#index"
 end
