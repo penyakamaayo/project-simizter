e99fde26d44114b40bbd5e77ed8f23efa09197a2
Tepen <elcabron.penyah@gmail.com>
2020-09-05 03:01:48 +0800

    changed localhost endpoint to live api endpoints

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,18 +1,3 @@
 class AccountsController < ApplicationController
   require 'open-uri'
-  def test
-    @data = JSON.parse(
-      open("http://localhost:1212/v1/accounts"
-      ).read, symbolize_names: true
-    )[:data]
-  end
-
-  def show
-    @id = params[:id]
-
-    @data = JSON.parse(
-      open("http://localhost:1212/v1/accounts/#{current_user.id}" 
-      ).read, symbolize_names: true
-    )[:data]
-  end
 end
